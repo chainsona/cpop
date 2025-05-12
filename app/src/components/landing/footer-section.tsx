cf9e2269c417ee518cba3c0a53e3c10f052196e7
Soona <code@chainsona.dev>
2025-05-12 03:46:03 +0200

    fix(footer): social links

--- app/src/components/landing/footer-section.tsx
+++ app/src/components/landing/footer-section.tsx
@@ -17,7 +17,7 @@ export function FooterSection() {
               </p>
               <div className="flex space-x-4">
                 <a
-                  href="#"
+                  href="https://x.com/chainsona"
                   className="text-neutral-600 hover:text-blue-600 dark:text-neutral-400 dark:hover:text-blue-400"
                 >
                   <span className="sr-only">Twitter</span>
@@ -27,11 +27,11 @@ export function FooterSection() {
                     viewBox="0 0 24 24"
                     aria-hidden="true"
                   >
-                    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+                    <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
                   </svg>
                 </a>
                 <a
-                  href="#"
+                  href="https://github.com/chainsona/cpop"
                   className="text-neutral-600 hover:text-blue-600 dark:text-neutral-400 dark:hover:text-blue-400"
                 >
                   <span className="sr-only">GitHub</span>
@@ -49,7 +49,7 @@ export function FooterSection() {
                   </svg>
                 </a>
                 <a
-                  href="#"
+                  href="https://discord.com/invite/qCv4Y7uYmh"
                   className="text-neutral-600 hover:text-blue-600 dark:text-neutral-400 dark:hover:text-blue-400"
                 >
                   <span className="sr-only">Discord</span>
